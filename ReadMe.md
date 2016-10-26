01d040b0b5e7a24583206f28183bc53f447ae7a6
HeskeyBaozi <hezhiyu233@foxmail.com>
2016-10-26 20:19:50 +0800

    add readme

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+# Table Sorter
+
+![sortable](./readmeImg/sortable.png)
+
+![sicily](./readmeImg/sicily.png)
+
+# License
+MIT
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@
 import $ from 'jquery';
 import {defineSortable, defineSortableAll} from './helper.js';
 
-module.exports = function Sorter(selector) {
+function Sorter(selector) {
     if (typeof selector === 'undefined') {
         defineSortableAll();
     } else if (typeof selector === 'string') {
         const $element = $(selector);
         defineSortable($element.children('thead'), $element.children('tbody'));
     }
-};
+}
+
+Sorter();
