00d693eb6b7160f5a9172a8cb8782d2584eb8bb4
HeskeyBaozi <hezhiyu233@foxmail.com>
2016-10-26 19:18:41 +0800

    reduce the line

--- src/index.js
+++ src/index.js
@@ -7,36 +7,55 @@ const Style = {
     decrease: 'decrease'
 };
 
+/**
+ * Attach the event for every table elements.
+ */
 $('table').each((item, element) => {
-    sortablefy($(element));
+    const $element = $(element);
+    defineSortable($element.children('thead'), $element.children('tbody'));
 });
 
-function sortablefy($table) {
-    $table.children('thead').click(e => {
-        const $tdOfThead = $(e.target);
-        $tdOfThead.addClass(Style.decrease)
-            .toggleClass(Style.increase)
-            .siblings()
-            .removeClass(Style.increase + ' ' + Style.decrease);
-        const isIncreasing = $tdOfThead.hasClass(Style.increase);
-        const $trows = $table.children('tbody').children('tr');
-        $trows.sort(compareFactory($tdOfThead.index(), isIncreasing))
-            .replaceAll('tbody tr');
-
+function defineSortable($tableHead, $tableBody) {
+    $tableHead.click(e => {
+        const $columnTitle = $(e.target);
+        toggleDecreaseClass($columnTitle);
+        const isIncreasing = $columnTitle.hasClass(Style.increase);
+        const $tableRows = $tableBody.children('tr').sort(compareFactory($columnTitle.index(), isIncreasing));
+        $tableBody.append($tableRows);
     });
 }
 
+function toggleDecreaseClass($columnTitle) {
+    $columnTitle.addClass(Style.decrease)
+        .toggleClass(Style.increase)
+        .siblings()
+        .removeClass(Style.increase + ' ' + Style.decrease);
+}
+
 function compareFactory(column, isIncreasing) {
-    const getTextContent = row => {
-        const text = $($(row).children('td')[column]).text();
-        return isNumber(text) ? parseFloat(text) : text;
+    const getTextContent = row => $($(row).children('td')[column]).text();
+    return (left, right) => {
+        left = getTextContent(left);
+        right = getTextContent(right);
+        let result = isNumber(left) && isNumber(right) ? numberCompare(left, right) : generalCompare(left, right);
+        return isIncreasing ? result : 0 - result;
     };
-    return (left, right) => (isIncreasing
-        ? getTextContent(left) > getTextContent(right)
-        : getTextContent(left) < getTextContent(right));
+}
+
+function numberCompare(leftNumber, rightNumber) {
+    return leftNumber - rightNumber;
+}
+
+function generalCompare(left, right) {
+    if (left < right) {
+        return -1;
+    } else if (left > right) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 function isNumber(text) {
     return /\d+\.?\d*/.test(text);
 }
-
