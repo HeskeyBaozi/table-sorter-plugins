d95a38c85d172b880ebbc4db68b01a15ad887b56
HeskeyBaozi <hezhiyu233@foxmail.com>
2016-10-27 13:39:24 +0800

    add comments

--- src/helper.js
+++ src/helper.js
@@ -1,11 +1,19 @@
 'use strict';
 
 import {isNumber, numberCompare, generalCompare} from './util.js';
+
+/**
+ * Style define.
+ * @type {{increase: string, decrease: string}}
+ */
 const Style = {
     increase: 'increase',
     decrease: 'decrease'
 };
 
+/**
+ * make all table sortable
+ */
 export function defineSortableAll() {
     $('table').each((item, element) => {
         const $element = $(element);
@@ -13,30 +21,63 @@ export function defineSortableAll() {
     });
 }
 
+/**
+ * make one table sortable
+ * @param $tableHead {Object}
+ * @param $tableBody {Array<Object>}
+ */
 export function defineSortable($tableHead, $tableBody) {
     $tableHead.click(e => {
         const $columnTitle = $(e.target);
-        toggleDecreaseClass($columnTitle);
-        const isIncreasing = $columnTitle.hasClass(Style.increase);
+        toggleIncreaseClass($columnTitle);
         $tableBody.children('tr')
-            .sort(compareFactory($columnTitle.index(), isIncreasing))
+            .sort(compareFactory($columnTitle.index(), isIncreasing($columnTitle)))
             .appendTo($tableBody);
     });
 }
 
-function toggleDecreaseClass($columnTitle) {
+/**
+ * make the increase style toggleable.
+ * @param $columnTitle
+ */
+function toggleIncreaseClass($columnTitle) {
     $columnTitle.addClass(Style.decrease)
         .toggleClass(Style.increase)
         .siblings()
         .removeClass(Style.increase + ' ' + Style.decrease);
 }
 
+/**
+ * create a compare function
+ * @param column {number}
+ * @param isIncreasing
+ * @return {function(*=, *=)}
+ */
 function compareFactory(column, isIncreasing) {
     const getTextContent = row => $($(row).children('td')[column]).text();
     return (left, right) => {
-        left = getTextContent(left);
-        right = getTextContent(right);
-        let result = isNumber(left) && isNumber(right) ? numberCompare(left, right) : generalCompare(left, right);
+        let result = [left, right]
+            .map(getTextContent)
+            .reduce(areBothNumber(left, right) ? numberCompare : generalCompare);
         return isIncreasing ? result : 0 - result;
     };
+}
+
+/**
+ * check the state if it is increasing.
+ * @param $columnTitle
+ * @return {boolean|*}
+ */
+function isIncreasing($columnTitle) {
+    return $columnTitle.hasClass(Style.increase);
+}
+
+/**
+ * check they are both number.
+ * @param left
+ * @param right
+ * @return {boolean}
+ */
+function areBothNumber(left, right) {
+    return isNumber(left) && isNumber(right);
 }
--- src/util.js
+++ src/util.js
@@ -1,10 +1,21 @@
 'use strict';
 
-
+/**
+ * number compare
+ * @param leftNumber
+ * @param rightNumber
+ * @return {number}
+ */
 export function numberCompare(leftNumber, rightNumber) {
     return leftNumber - rightNumber;
 }
 
+/**
+ * general compare
+ * @param left
+ * @param right
+ * @return {number}
+ */
 export function generalCompare(left, right) {
     if (left < right) {
         return -1;
@@ -15,6 +26,11 @@ export function generalCompare(left, right) {
     }
 }
 
+/**
+ * check if it is number
+ * @param text
+ * @return {boolean}
+ */
 export function isNumber(text) {
     return /^\d+(\.\d+)?$/.test(text.trim());
 }
